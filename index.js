79e52ee9ce8f49278bdfd1198699e505a6485326
Jason Belcher <jason.belcher@cumberlandgroupit.com>
2019-03-24 17:43:24 -0400

    remove test route reference causing error on start

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
-app.use('/api/test', testRoutes);
+
 
 app.use(function(req, res, next) {
   let err = new Error('Not Found');
